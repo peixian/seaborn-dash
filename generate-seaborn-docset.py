a12ed9862a4cc60bc6a661ba642eda0ec9a3c2f8
Peixian Wang <peixian@users.noreply.github.com>
2016-08-29 22:04:11 -0500

    Finish script

--- generate-seaborn-docset.py
+++ generate-seaborn-docset.py
@@ -1,2 +1,22 @@
+from bs4 import BeautifulSoup as BS
 from subprocess import call
+import sqlite3
 
+call(['bash', 'helper.sh'])
+with open('Seaborn.docset/Contents/Resources/Documents/examples/index.html', 'r') as infile:
+    examples = BS(infile, 'lxml').find('div', {'id': 'example-gallery'}).find_all('a')[1:]
+    gallaries = [] #list of examples, in the format of (name, link)
+    dash_str_format= 'examples/{0}'
+    for example in examples:
+        name = example.span.p.text
+        link = dash_str_format.format(example['href'][2:])
+        gallaries.append((name, link))
+
+conn = sqlite3.connect('Seaborn.docset/Contentes/Resources/docSet.dsidx')
+c = conn.cursor()
+
+for item in gallaries:
+    c.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?, ?, ?)', (item[0], 'Sample', item[1]))
+
+conn.commit()
+conn.close()
--- helper.sh
+++ helper.sh
@@ -1,1 +1,13 @@
-#! /bin/bash
+#!/bin/bash
+
+echo "Pulling Seaborn";
+git clone "https://github.com/mwaskom/seaborn/";
+cd seaborn/doc;
+cd tutorial;
+make;
+cd ..;
+make html;
+doc2dash -n Seaborn _build/html;
+mv Seaborn.docset ../../Seaborn.docset;
+cd ../../;
+rm -rf seaborn;
